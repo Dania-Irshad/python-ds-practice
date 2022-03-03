9b6931e518f8ff19d2ead62c76a4dbf14744e989
Dania Irshad <daniairshad@live.com>
2022-03-02 18:56:37 -0600

    fixed out of range error

--- 02_weekday_name/weekday_name.py
+++ 02_weekday_name/weekday_name.py
@@ -14,7 +14,7 @@ def weekday_name(day_of_week):
     """
     days = {1: 'Sunday', 2: 'Monday', 3: 'Tuesday', 4: 'Wednesday', 5: 'Thursday', 6: 'Friday', 7: 'Saturday'}
 
-    if day_of_week >= 1 or day_of_week <= 7:
-        return days[day_of_week]
-    else:
+    if day_of_week < 1 or day_of_week > 7:
         return None
+    else:
+        return days[day_of_week]
